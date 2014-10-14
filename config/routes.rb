ab5671bfb1326cdcb0189c2441d0d58858f0915a
Kristin Gonzalez <kristin_gonzalez@yahoo.com>
2014-10-14 11:31:10 +0800

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Blocipedia::Application.routes.draw do
+
+  devise_for :users
+  root to: 'welcome#index'
+
+end
---
+++ spec/features/user_signs_up_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'User signs up for Blocipedia' do
+  scenario 'Successfully' do
+    visit new_user_registration_path
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password', match: :prefer_exact
+    click_button 'Sign up'
+    expect(page).to have_content('A message with a confirmation link has been sent to your email address. Please open the link to activate your account.')
+    #expect(page).to have_content('Welcome to Bloccitoff')
+  end
+end
+
+
+#As a user, I should be able to sign up for a 
+#free account by providing a user name, password and email address.
