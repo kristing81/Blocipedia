8a45199242a714e74b5fcd4c8ae95ccff6207dc1
Kristin Gonzalez <kristin_gonzalez@yahoo.com>
2014-10-19 21:55:38 +0800

    updating rolescontroller

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -2,10 +2,10 @@ class Wiki < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: [:slugged, :history, :finders]
 
+  has_many :roles
+  has_many :users, through: :roles
+  
   # def should_generate_new_friendly_id?
   #   new_record?
   # end
-
-  has_many :roles
-  has_many :users, through: :roles
 end
