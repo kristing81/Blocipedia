54b46699ba4a52cdc01238668f366a56eeca62ea
Kristin Gonzalez <kristin_gonzalez@yahoo.com>
2014-11-02 17:07:14 +0800

    secret token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blocipedia::Application.config.secret_key_base = 'c2eebf1d05ac7eaea480dab96aa2d556acec7cb1542dd135444cfad6336c852a7d466ccf3c6a1dec1430b3182804c7a6a03d7e63181854336376802c1fcfbacd'
