8cd000bcc7f5952b736ea8bec850a57fc3ca7d69
Kristin Gonzalez <kristin_gonzalez@yahoo.com>
2014-11-17 19:19:21 +0800

    JS file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,23 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+var blocmetrics = function (tracked_domain, event_type) {
+    var _bm_event = {
+        event : {
+        tracked_domain: tracked_domain,
+        event_type: event_type
+        }
+    }
+
+    var _bm_request = new XMLHttpRequest();
+    _bm_request.open("POST", "//kristingonzalez-blocmetrics.herokuapp.com/api/v1/events.json", true);
+    _bm_request.setRequestHeader('Content-Type', 'application/json');
+    _bm_request.onreadystatechange = function () {
+
+    };
+
+
+    _bm_request.send(JSON.stringify(_bm_event));
+};
+
+blocmetrics('tracked_domain', 'event_type')
